5aab26d9fd966c47bc3bbbbada4a039b5322974d
hridoy264 <hridoyq264@gmail.com>
2026-07-30 22:01:15 +0600

    Indentation error of main function is corrected

--- picka_kinematics/fk_server.py
+++ picka_kinematics/fk_server.py
@@ -67,16 +67,15 @@ class FKServer(Node):
 
         return response
 
-    def main(args=None):
-        rclpy.init(args=args)
-        node = FKServer()
-        try:
-            rclpy.spin(node)
-        except KeyboardInterrupt:
-            pass
-        finally:
-            node.destroy_node()
-            rclpy.shutdown()
-
-    if __name__=='__main__':
-        main()
+def main(args=None):
+    rclpy.init(args=args)
+    node = FKServer()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+if __name__=='__main__':
+    main()
